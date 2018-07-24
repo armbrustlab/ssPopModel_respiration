9512b9f6b347815457050b3a71bab3478c07e260
John MacMillan <johnmacmillan96@gmail.com>
2018-07-24 16:43:01 -0700

    Added delta plot

--- plot.param.mat.R
+++ plot.param.mat.R
@@ -24,14 +24,15 @@ path.to.data <- "~/ssPopModel_sensitivity_test/ssPopModel_sensitivity_test_data"
 path.to.git.repository <- "~/ssPopModel_sensitivity_test"
 setwd(path.to.git.repository)
 
-list.dist  <- list.files("input", "size.distribution_Prochlorococcus",full.names=T)
-load(list.dist[1])
-plot.size.distribution(distribution[[1]][37:61], type='l', lwd=2)
+#list.dist  <- list.files("input", "size.distribution_Prochlorococcus",full.names=T)
+#load(list.dist[1])
+#plot.size.distribution(distribution[[1]][37:61], type='l', lwd=2)
 
 
 ## MERGE MODEL OUTPUT (dt)
-list.output <- list.files("output", "size",full.names=T)
-DF <- NULL
+list.output <- list.files("output_matrix", "size",full.names=T)
+
+params.all <- NULL
 
 for(path.distribution in list.output){
     #path.distribution <- list.output[1]
@@ -42,64 +43,69 @@ for(path.distribution in list.output){
     size <- as.numeric(file.name[3])
     dt <- as.numeric(file.name[4])
 
-    # if(origin == "biomass"){
-    #     params <- model2[,2][[1]]
-    #     gr <- model2[,2][[2]]
-    # }
-
-    if(origin == "size"){
-        params <- model1[,2][[1]]
-        gr <- model1[,2][[2]]
-    }
-    df <- data.frame(origin, size, dt, params,gr=sum(gr,na.rm=T))
-    DF <- data.frame(rbind(DF, df))
+    # get the parameters
+    params <- model1[,2][[1]]
+    gr <- model1[,2][[2]]
+    # save all the parameters
+    temp <- data.frame(origin, size, dt, params,gr=sum(gr,na.rm=T))
+    params.all <- data.frame(rbind(params.all, temp))
+    # i <- i + 1
 }
 
-
-path.distribution <- list.output[100]
-load(path.distribution)
-summary(path.distribution)
+params.all <- params.all[order(as.numeric(params.all$dt)),]
 
 
+################ PLOT parameters
 
-function (merged.estimates)
-{
-    jet.colors <- colorRampPalette(c("#00007F", "blue", "#007FFF",
-        "cyan", "#7FFF7F", "yellow", "#FF7F00", "red", "#7F0000"))
-    volbins <- unique(as.numeric(row.names(merged.estimates$Vproj)))
-    para <- merged.estimates$estimates
-    h2.time <- para$h.time
-    cat <- length(volbins)
-    del <- matrix(nrow = length(h2.time), ncol = cat)
-    for (i in 1:cat) del[, i] <- para$h2.dmax.mean * (volbins[i]/max(volbins))^para$h2.b.mean/(1 +
-        (volbins[i]/max(volbins))^para$h2.b.mean)
-    par(mfrow = c(2, 1), mar = c(4, 4, 4, 4), las = 1)
-    plot(volbins, del[1, ], ylim = c(0, max(del, na.rm = T)),
-        type = "l", col = "#00007F", lwd = 2, xlab = "Cell volume",
-        ylab = paste("Delta (per", 10, "min)"))
-    for (i in 2:nrow(del)) points(volbins, del[i, ], type = "l",
-        col = jet.colors(nrow(del))[cut(as.numeric(h2.time),
-            nrow(del))][i], lwd = 2)
-    ylim <- par("usr")[c(3, 4)]
-    xlim <- par("usr")[c(1, 2)]
-    color.legend(xlim[2] - diff(xlim)/40, ylim[1], xlim[2], ylim[2],
-        legend = format(as.POSIXct(range(h2.time, na.rm = T),
-            origin = "1970-01-01"), "%d %b"), rect.col = jet.colors(100),
-        gradient = "y", align = "rb")
-    max <- max(para$h2.gmax.mean * (1 - exp(-1000)/para$h2.E_star.mean),
-        na.rm = T)
-    plot(seq(0, 1000, by = 10), para$h2.gmax.mean[1] * (1 - exp(-seq(0,
-        1000, by = 10)/para$h2.E_star.mean[1])), ylim = c(0,
-        max), type = "l", col = "#00007F", lwd = 2, xlab = "Light Intensity",
+# Colors
+colfunc <- colorRampPalette(c("red", "blue"))
+color <- colfunc(length(params.all$dmax))
+
+# Get the par data
+Par <- read.csv("Par.csv")
+
+# get the max delta
+max <- max(params.all$gmax * (1 - exp(-1000)/params.all$E_star),
+    na.rm = T)
+
+# plot the first delta
+plot(seq(0, max(Par$par), by=10),
+    params.all$gmax[1] * (1 - exp(-seq(0, max(Par$par), by=10)/params.all$E_star[1])),
+    ylim = c(0, max), type = "l", col = "red",
+    lwd = 0.1, xlab = "Light Intensity",
+    ylab = paste("Gamma (per", 10, "min)"))
+
+# plot the rest of the delta's
+for (i in 2:length(params.all$gmax)) {
+
+    #if (i %% 50 != 0) next
+
+    points(seq(0, max(Par$par), by=10),
+        params.all$gmax[i] * (1 - exp(-seq(0, max(Par$par), by=10)/params.all$E_star[i])),
+        ylim = c(0, max), type = "l", col = color[i],
+        lwd = 0.1, xlab = "Light Intensity",
         ylab = paste("Gamma (per", 10, "min)"))
-    for (i in 1:length(h2.time)) points(seq(0, 1000, by = 10),
-        para$h2.gmax.mean[i] * (1 - exp(-seq(0, 1000, by = 10)/para$h2.E_star.mean[i])),
-        type = "l", col = jet.colors(nrow(del))[cut(as.numeric(h2.time),
-            length(h2.time))][i], lwd = 2)
-    ylim <- par("usr")[c(3, 4)]
-    xlim <- par("usr")[c(1, 2)]
-    color.legend(xlim[2] - diff(xlim)/40, ylim[1], xlim[2], ylim[2],
-        legend = format(as.POSIXct(range(h2.time, na.rm = T),
-            origin = "1970-01-01"), "%d %b"), rect.col = jet.colors(100),
-        gradient = "y", align = "rb")
+
+
+}
+
+# We get the range of the volbin sizes from one of the models,
+# the range is the same for all runs of the model
+range <- range(as.numeric(row.names(model1[3,][[2]])))
+
+volbins <- seq(range[1], range[2], length.out=100)
+
+delta <- matrix(nrow = length(params.all$gr), ncol = length(volbins))
+
+
+for (i in 1:length(volbins)) {
+    delta[,i] <- params.all$dmax *
+        ((volbins[i] / max(volbins))^params.all$b /
+        ((1 + volbins[i]/max(volbins))^params.all$b))
+}
+
+plot(volbins, delta[1,], ylim=c(0, max(delta, na.rm=T)),type='l', lwd=0.1, col = "red")
+
+for (i in 2:length(params.all$dmax)) {
+    points(volbins, delta[i,], ylim=c(0, max(delta, na.rm=T)),type='l', lwd=0.1, col=color[i])
 }
