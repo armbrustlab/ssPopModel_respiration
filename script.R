95cfc1bc2620e005cadde5bf51dd0bbb783cd56c
Francois Ribalet <ribalet@uw.edu>
2018-08-08 15:32:28 -0700

    add smoothed size distriubution

--- script.R
+++ script.R
@@ -10,8 +10,8 @@ path.to.data <- "~/ssPopModel_sensitivity_test/ssPopModel_sensitivity_test_data"
 path.to.git.repository <- "~/ssPopModel_sensitivity_test"
 
 #Francois
-path.to.data <- "~/Documents/DATA/Codes/ssPopModel_sensitivity_test_data"
 path.to.git.repository <- "~/Documents/DATA/Codes/ssPopModel_sensitivity_test"
+path.to.data <- paste0(path.to.git.repository,"/ssPopModel_sensitivity_test_data")
 
 
 
@@ -20,19 +20,19 @@ path.to.git.repository <- "~/Documents/DATA/Codes/ssPopModel_sensitivity_test"
 # #1. to download the data (using DAT)
 # dat://456f261260e4ae8af7e7dc8b97fdabfdc770f561771e844888b1ef7f59a507ec
 #
-# #2. calculate size distrubution
-# setwd(path.to.data)
-# popname <- "prochloro"
-# time.interval <- 60 #minutes
-# opp.dir <- "SCOPE_6_opp"
-# vct.dir <- "SCOPE_6_vct"
-# db <- "SCOPE_6.db"
-# inst <- "740"
-#
-# for(width in seq(0.045,0.10,by=0.001)){
-#   distribution <- size.distribution(db, opp.dir, vct.dir, popname=popname, volume.width=width, time.interval = time.interval)
-#   save(distribution,file=paste0(path.to.git.repository,"/input/size.distribution_Prochlorococcus_",width))
-# }
+#2. calculate size distrubution
+setwd(path.to.data)
+popname <- "prochloro"
+time.interval <- 60 #minutes
+opp.dir <- "SCOPE_6_opp"
+vct.dir <- "SCOPE_6_vct"
+db <- "SCOPE_6.db"
+inst <- "740"
+
+for(width in seq(0.045,0.10,by=0.001)){
+  distribution <- size.distribution(db=db, opp.dir=opp.dir, vct.dir=vct.dir, popname=popname, volume.width=width, time.interval = time.interval)
+  save(distribution,file=paste0(path.to.git.repository,"/input_smooth/size.distribution_Prochlorococcus_",width))
+}
 
 
 
@@ -40,55 +40,36 @@ setwd(path.to.git.repository)
 Par <- read.csv("Par.csv")
 
 
-## ESTIMATE GROWTH RATE
-list.dist  <- list.files("input", "size.distribution_Prochlorococcus",full.names=T)
+## ESTIMATE GROWTH RATE for each width and various dt
+list.dist  <- list.files("input_smooth", "size.distribution_Prochlorococcus",full.names=T)
 
-for(path.distribution in list.dist){
-          #path.distribution <- list.dist[1]
-          print(path.distribution)
-          load(path.distribution)
-          size <- unlist(list(strsplit(path.distribution, "_")))[3]
-          freq.distribution2 <- distribution[[2]][37:61] # biomass data for 1 day only
-          freq.distribution1 <- distribution[[1]][37:61] # size data for 1 day only
+for(dt in seq(5, 20, by=0.5)) {
+    print(paste0("Time = ", dt))
 
-          #plot.size.distribution(freq.distribution2, mode="log", type="l", lwd=2)
+    for(path.distribution in list.dist){
+              #path.distribution <- list.dist[55]
+              print(path.distribution)
+              load(path.distribution)
+              size <- unlist(list(strsplit(path.distribution, "_")))[4]
+              freq.distribution <- as.matrix(distribution[[1]][37:61]) # size data for 1 day only
+              Ntot <- as.vector(distribution[[2]][37:61])
+              t <- 1
+                    # calculating division rate based on size
+                model1 <- run.ssPopModel(freq.distribution, Ntot, Par, time.delay=t, dt=dt)
+                  save(model1, file=paste0('output_smooth/size_modeloutput_',size,"_dt_",dt))
 
-          Ntot <- distribution[[3]][37:61]
-          t <- 1
-            # 1. calculating division rate based on biomass
-            model2 <- run.ssPopModel(freq.distribution2, Ntot, Par, time.delay=t, dt=10)
-              save(model2, file=paste0('output/biomass_modeloutput_',size))
-            # 2. calculating division rate based on size
-            model1 <- run.ssPopModel(freq.distribution1, Ntot, Par, time.delay=t, dt=10)
-              save(model1, file=paste0('output/size_modeloutput_',size))
-
+                  # n.distribution <- freq.distribution %*% diag(Ntot)
+                  # colnames(n.distribution) <- colnames(freq.distribution)
+                  #plot.size.distribution(freq.distribution, mode="log", type="p", lwd=2)
+                  #plot.size.distribution(n.distribution, mode="log", type="p", lwd=2)
+    }
 }
 
 
 
-## Run model varying dt, width at 0.10
-
-list.dist.10 <- list.dist[59]
 
 
-    for(dt in seq(3, 30, by=1)) {
-        print(paste0("Time = ", dt))
-        load(list.dist.10)
-        freq.distribution2 <- distribution[[2]][37:61] # biomass data for 1 day only
-        freq.distribution1 <- distribution[[1]][37:61] # size data for 1 day only
 
-        #plot.size.distribution(freq.distribution2, mode="log", type="l", lwd=2)
-
-        Ntot <- distribution[[3]][37:61]
-        t <- 1
-
-        # 1. calculating division rate based on biomass
-        model2 <- run.ssPopModel(freq.distribution2, Ntot, Par, time.delay=t, dt=dt)
-        save(model2, file=paste0('output/biomass_modeloutput_0.10_dt=', dt))
-        # 2. calculating division rate based on size
-        model1 <- run.ssPopModel(freq.distribution1, Ntot, Par, time.delay=t, dt=dt)
-        save(model1, file=paste0('output/size_modeloutput_0.10_dt=', dt))
-    }
 
 
 
